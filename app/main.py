c27c2316aa9625a6890408e3cbace775cebfc7c3
Koresh <koresh22217@mail.ru>
2024-05-20 23:23:01 +0300

    mai

--- app/main.py
+++ app/main.py
@@ -19,4 +19,8 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app")
+    uvicorn.run(
+        "main:app", 
+        host=settings.run.host,
+        port=settings.run.port,
+    )

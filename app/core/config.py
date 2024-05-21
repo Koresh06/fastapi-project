59232c07831543088f5e620e3d9ae1f8fe0047bb
Koresh <koresh22217@mail.ru>
2024-05-21 23:19:58 +0300

    add db_helper

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
 class RunConfig(BaseModel):
-    host: str = "127.0.0.1"
-    port: int = 8000
+    host: str = "0.0.0.0"
+    port: int = 8080
 
 
 class ApiConfig(BaseModel):
     prefix: str = "/api"
 
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
+
 class Sttings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiConfig = ApiConfig()
-
+    db: DatabaseConfig
 
 
 settings = Sttings()
